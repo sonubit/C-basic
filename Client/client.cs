a2e94657cfdeae692ae3a4fdfb7bc3d5496971cc
sonubit <sonucs0128@gmail.com>
2020-08-20 11:39:55 +0530

    19/08/2020

---
+++ Client/client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Client
+    { 
+        
+        public static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("localhost", 8888);
+            NetworkStream ns = client.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            Console.WriteLine("server ");
+            Console.WriteLine("server >" +sr.ReadToEnd());
+            /*StreamWriter sw = new StreamWriter(ns);
+            Console.WriteLine("text something here");
+            sw.WriteLine("Console.ReadLine()");
+            sw.Flush();*/
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace Server
+{
+    class Server
+    {
+    public static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(8888);
+            server.Start();
+            Console.WriteLine("waiting for client");
+            Socket socketForClients = server.AcceptSocket();
+
+            if (socketForClients.Connected)
+            {
+                NetworkStream ns = new NetworkStream(socketForClients);
+                StreamWriter sw = new StreamWriter(ns);
+                Console.WriteLine("server > welcome client");
+                sw.WriteLine("welcome client");
+                sw.WriteLine("hi client");
+                sw.Flush();
+                /*StreamReader sr = new StreamReader(ns);
+                Console.WriteLine("client >" + sr.ReadLine());*/
+                sw.Close();
+                ns.Close();
+            }
+            socketForClients.Close();
+
+        }
+    }
+}
